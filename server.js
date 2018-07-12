01eb32f8a87ff58acdfc7b536e97c2583f0877a9
hesham <heshamelmasry1992@gmail.com>
2018-07-12 14:43:22 +0200

    cleaning

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-'use strict';
+"use strict";
 
-const express = require('express');
-const path = require('path');
-const bodyParser = require('body-parser');
+const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser");
 const app = express();
 const port = process.env.PORT || 7000;
 
@@ -11,13 +11,11 @@ app.use(bodyParser.json()); // <--- Here
 app.use(bodyParser.urlencoded({extended: true})); // handle URL-encoded data
 
 
+app.use(express.static(path.resolve(__dirname, "./react-ui/build")));
 
 
-app.use(express.static(path.resolve(__dirname, './react-ui/build')));
-
-
-app.get('*', function(request, response) {
-  response.sendFile(path.resolve(__dirname, './react-ui/build', 'index.html'));
+app.get("*", function (request, response) {
+	response.sendFile(path.resolve(__dirname, "./react-ui/build", "index.html"));
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
