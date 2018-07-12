3944830f5c8edc5fdb5d12a49a92638d727a3087
hesham <heshamelmasry1992@gmail.com>
2018-07-12 15:30:48 +0200

    design for the count down
    1. new styles for the count down boxes as per design.
    2. adding a btn for submitting the date of black friday in future.

--- react-ui/src/index.js
+++ react-ui/src/index.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap/dist/css/bootstrap-theme.css';
 // Put any other imports below so that CSS from your
 // components takes precedence over default styles.
 import './stylesheets/index.css';
