b93b133da7e015f79163f25813579791940271b4
hesham <heshamelmasry1992@gmail.com>
2018-07-13 12:48:14 +0200

    new clock component
    new compoment for the clock, moving some css and html , new state for the clock values

---
+++ react-ui/src/components/Clock/index.js
@@ -0,0 +1,36 @@
+import React from "react";
+import {Component} from "react";
+// eslint-disable-next-line
+import "./styles/Clock.css";
+
+class Clock extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			days:0,
+			hours:0,
+			minutes:0,
+			seconds:0
+		};
+	}
+	
+	
+	render() {
+		return (
+			<div className="Clock">
+				<div className="values-container">
+					<div className="clock-days"><span className="date-values">{this.state.days}</span><span
+						className="date-names">days</span></div>
+					<div className="clock-hours"><span className="date-values">{this.state.hours}</span> <span
+						className="date-names">hours</span></div>
+					<div className="clock-minutes"><span className="date-values">{this.state.minutes}</span> <span
+						className="date-names">minutes</span></div>
+					<div className="clock-seconds"><span className="date-values">{this.state.seconds}</span> <span
+						className="date-names">seconds</span></div>
+				</div>
+			</div>
+		);
+	}
+}
+
+export default Clock;
